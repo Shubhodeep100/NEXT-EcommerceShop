e2fe34be138d6cf677b3a0e415f2dec77c102a53
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-09 18:10:22 +0530

    product slug work

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import ImageGallery from "@/app/components/ImageGallery";
 import { client } from "@/app/lib/sanity";
 import { fullProduct } from "@/app/interface";
+import { Button } from "@/components/ui/button";
+import { Star } from "lucide-react";
 
 async function getData(slug: string) {
     const query = `*[_type == "product" && slug.current == "${slug}"][0]{
@@ -30,11 +32,21 @@ export default async function ProductPage({
                 <div className="grid gap-8 md:grid-cols-2">
                     <ImageGallery images={data.images} />
                     <div className="md:py-8">
-                    <div className="mb-2 md:mb-3">
-                    <span>
-                        {data.categoryName}
-                    </span>
-                    </div>
+                        <div className="mb-2 md:mb-3">
+                            <span className="mb-0.5 inline-block text-gray-500">
+                                {data.categoryName}
+                            </span>
+                            <h2 className="text-2xl font-bold">{data.name}</h2>
+                        </div>
+                        <div className="mb-6 flex items-center gap-3 md:mb-10">
+                            <Button className="rounded-full gap-x-2">
+                                <span className="text-sm">4.2</span>
+                                <Star className="h-4 w-4"/>
+                            </Button>
+                            <span className="text-sm text-gray-500 transition duration-100">
+                                56 Rating
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
