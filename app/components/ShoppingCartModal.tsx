d4b859716a97ca9c5417029f9dd9959ba1405ba1
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-10 12:20:38 +0530

    category modal work

---
+++ app/components/ShoppingCartModal.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+export default function ShoppingCartModal() {
+    return (
+        <Sheet>
+
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Edit profile</SheetTitle>
+                    <SheetDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </SheetDescription>
+                </SheetHeader>
+
+                <SheetFooter>
+                    <SheetClose asChild>
+                        <Button type="submit">Save changes</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
+    )
+}
