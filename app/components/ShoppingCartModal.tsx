9cf61ea29f95a4c18c53156c1ac08b55240ef43f
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-10 13:02:05 +0530

    shoppingcart modal work

--- app/components/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal.tsx
@@ -5,9 +5,10 @@ import {
     SheetHeader,
     SheetTitle,
 } from "@/components/ui/sheet"
+import Image from "next/image";
 import { useShoppingCart } from "use-shopping-cart";
 export default function ShoppingCartModal() {
-    const { cartCount, shouldDisplayCart, handleCartClick } = useShoppingCart()
+    const { cartCount, shouldDisplayCart, handleCartClick, cartDetails } = useShoppingCart()
     return (
         <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
 
@@ -20,7 +21,15 @@ export default function ShoppingCartModal() {
                     <div className="mt-8 flex-1 overflow-y-auto">
                         <ul className="-my-6 divide-y divide-gray-200">
                             {cartCount === 0 ? (<h1 className="py-6">You dont have any items</h1>) : (
-                                <h1 >Hey! You have some items.</h1>
+                                <>
+                                    {Object.values(cartDetails ?? {}).map((entry) => (
+                                        <li key={entry.id} className="flex py-6">
+                                            <div className="flex py-6 overflow-hidden rounded-md border border-gray-200">
+                                                <Image src={entry.image as string} alt="Product Image" width={100} height={100} />
+                                            </div>
+                                        </li>
+                                    ))}
+                                </>
                             )}
                         </ul>
                     </div>
